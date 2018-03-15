adc1d94b6d306ff4615792466f363e34c8e9bc45
kalenne <kalenne.p@gmail.com>
2018-03-15 10:54:11 -0300

    Primeiro StringBoot

---
+++ src/main/java/com/aulapablo/p5spring/AulaSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.aulapablo.p5spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication /*se nao tiver isso, num rola*/
+public class AulaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringBootApplication.class, args); /* rolar*/
+		
+		
+	}
+}
